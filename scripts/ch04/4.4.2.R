f152baa322763ebe9addd5c9bb99771a21b86a31
Yusuke_Saito <ysaito8015@gmail.com>
2023-03-01 21:07:57 +0900

    4.4.2

---
+++ scripts/ch04/4.4.2.R
@@ -0,0 +1,25 @@
+source("environments.R")
+.libPaths(libDir)
+figDir <- file.path(outDir, "ch04")
+if (!dir.exists(figDir)) {
+    dir.create(figDir)
+}
+
+pacman::p_load(
+    tidyverse,
+    rio
+)
+
+data <-
+    rio::import(file.path(dataDir, "data-salary.txt"))
+
+p <-
+    data %>%
+    ggplot() +
+    aes(x = X, y = Y) +
+    geom_point()
+
+ggsave(
+    file.path(figDir, "fig.4.2.pdf"),
+    plot = p
+)
