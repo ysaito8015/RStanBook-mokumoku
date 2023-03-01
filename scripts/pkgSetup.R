1ef1687ac8243c6f8b51ff6d649e0269d7edfe63
Yusuke_Saito <ysaito8015@gmail.com>
2023-03-01 15:40:49 +0900

    seup packages

---
+++ scripts/pkgSetup.R
@@ -0,0 +1,39 @@
+pkgs <-
+    c(
+        "tidyverse",
+        "bayesplot",
+        "ggmcmc",
+        "rstan",
+        "pacman",
+        "rio"
+)
+
+source("./environments.R")
+.libPaths(libDir)
+
+
+install.packages(
+    c("devtools", "BH", "remotes"),
+    lib = libDir,
+    contriburl = c(
+        contrib.url("http://r-forge.r-project.org","source"),
+        contrib.url("https://cran.rstudio.com/","source")
+    ),
+    destdir = pkgDir,
+    type = "source",
+    dependencies = c("Depends", "Imports", "LinkingTo"),
+    INSTALL_opts = "--no-multiarch"
+)
+
+install.packages(
+    pkgs,
+    lib = libDir,
+    contriburl = c(
+        contrib.url("http://r-forge.r-project.org","source"),
+        contrib.url("https://cran.rstudio.com/","source")
+    ),
+    destdir = pkgDir,
+    type = "source",
+    dependencies = c("Depends", "Imports", "LinkingTo"),
+    INSTALL_opts = "--no-multiarch"
+)
