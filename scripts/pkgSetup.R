4b30dae23a40874efd115ee48e290aadd5076f4d
Yusuke_Saito <ysaito8015@gmail.com>
2023-03-04 12:26:52 +0900

    update setup scripts

---
+++ scripts/pkgSetup.R
@@ -0,0 +1,60 @@
+pkgs <-
+  c(
+    "RcppParallel",
+    "tidyverse",
+    "bayesplot",
+    "ggmcmc",
+    "patchwork",
+    "mrgsolve",
+    "pacman",
+    "rio"
+)
+
+stan_pkgs <-
+  c(
+    "StanHeaders",
+    "rstan"
+  )
+
+source("./environments.R")
+.libPaths(libDir)
+
+
+install.packages(
+  c("devtools", "BH", "remotes"),
+  lib = libDir,
+  contriburl = c(
+    contrib.url("http://r-forge.r-project.org","source"),
+    contrib.url("https://cran.rstudio.com/","source")
+  ),
+  destdir = pkgDir,
+  type = "source",
+  dependencies = c("Depends", "Imports", "LinkingTo"),
+  INSTALL_opts = "--no-multiarch"
+)
+
+install.packages(
+  pkgs,
+  lib = libDir,
+  contriburl = c(
+    contrib.url("http://r-forge.r-project.org","source"),
+    contrib.url("https://cran.rstudio.com/","source")
+  ),
+  destdir = pkgDir,
+  type = "source",
+  dependencies = c("Depends", "Imports", "LinkingTo"),
+  INSTALL_opts = "--no-multiarch"
+)
+
+install.packages(
+  stan_pkgs,
+  lib = libDir,
+  contriburl = c(
+    contrib.url("https://mc-stan.org/r-packages/","source"),
+    contrib.url("http://r-forge.r-project.org","source")
+  ),
+  destdir = pkgDir,
+  type = "source",
+  dependencies = c("Depends", "Imports", "LinkingTo"),
+  INSTALL_opts = "--no-multiarch"
+)
